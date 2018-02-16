0f233e70ec9beaacc5523643df3f50d102833dc6
Maciej Malkowski <maciej.malkowski@agoda.com>
2018-02-16 18:18:24 +0700

    refactor assetsMapper to use reduce instead

--- src/assetsUtils/assetsMapper.js
+++ src/assetsUtils/assetsMapper.js
@@ -3,15 +3,13 @@
 const _ = require('lodash');
 
 const map = function(stats) {
-    const mappedAssets = {};
-    stats.assets.forEach(a => {
-        const chunkName = _.head(a.chunkNames);
-        const extention = a.name.split('.').pop();
-        const name = chunkName ? `${chunkName}.${extention}` : a.name;
-
-        mappedAssets[name] = a.size;
-    });
-    return mappedAssets;
+    return stats.assets.reduce((acc, current) => {
+        const chunkName = _.head(current.chunkNames);
+        const extention = current.name.split('.').pop();
+        const name = chunkName ? `${chunkName}.${extention}` : current.name;
+        acc[name] = current.size;
+        return acc;
+    }, {});
 };
 
 const mapStatsComparison = function (oldAssetSize, newAssetSize) {
